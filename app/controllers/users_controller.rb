7e6b833227158af595718cb51ad0aa2becf25a8e
Jonathan Hong <jonhong777@gmail.com>
2018-08-23 21:13:24 +0000

    Create log in page and the post request route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
     session[:user_id] = @user.id
     redirect to "#"
   end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/textbooks'
+    else
+      redirect to '/login'
+    end
+  end
 end
